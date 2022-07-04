20b197f43bea1e07cfea9efe46ed29d23955f518
RayDev1988 <devsops2022@gmail.com>
2022-07-04 09:16:27 -0700

    Header-update

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,21 +3,7 @@ const Header = () => {
   return (
     <nav className="header">
       <div className="logo">
-        <img src="/assets/blog/aurthor (2).jpeg" />
-      </div>
-      <div className="nav-item">
-        <span>
-          <Link href="/">Home</Link>
-        </span>
-        <span>
-          <Link href="/episodes">Episodes</Link>
-        </span>
-        <span>
-          <Link href="/season2">Season 2</Link>
-        </span>
-        <span>
-          <Link href="/quotes">Quotes</Link>
-        </span>
+        <img src="/assets/gradient.png" className="logo-img" />
       </div>
     </nav>
   );
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,7 @@ function HomePage(props) {
   return (
     <section className="home">
       <Header />
-      <img src={"/HomePage.jpg"} alt="Home Page" className="absolute top-6" />
-      <p>
-        photo can originally be found{" "}
-        <a href="https://starwars.fandom.com/wiki/The_Mandalorian">here</a>
-      </p>
+      <img src={"/assets/image (24).jpg"} alt="Home Page" className="home-img" />
     </section>
   );
 }
